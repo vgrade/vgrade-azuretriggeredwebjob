9e32ac2eb71b01516f197fbcb03bb0e404d1ab17
kumar.vishal@azdcs.onmicrosoft.com <vishal.grade@hp.com>
2020-07-20 22:46:28 +0530

    Add project files.

---
+++ WebJob5/Functions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace WebJob5
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+      
+        public static void Run([TimerTrigger("%myJobschedule%")]TimerInfo myTimer, TextWriter log)
+        {
+            log.WriteLine("VishalTimer is running late!");
+           // if (myTimer.IsPastDue)
+            {
+                log.WriteLine("Timer is running late!");
+            }
+            log.WriteLine($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+    }
+}
---
+++ WebJob5/ScheduleExpressionResolver.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.WebJobs;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebJob5
+{
+    public class ScheduleExpressionResolver : INameResolver
+    {
+        public string Resolve(string name)
+        {
+            return ConfigurationManager.AppSettings[name];
+        }
+    }
+}
